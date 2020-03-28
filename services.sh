cc63f1876255e6d5cd6b30172011290d9439ad90
Stalker291087 <jeancarloe01@hotmail.com>
2020-03-28 16:38:51 -0600

    Adding services restart and crontab backup

---
+++ services.sh
@@ -0,0 +1,14 @@
+## Service Status:
+sudo /opt/bitnami/ctlscript.sh status
+
+## Start all services:
+sudo /opt/bitnami/ctlscript.sh start
+
+## Stop all services:
+sudo /opt/bitnami/ctlscript.sh stop
+
+## Restart all services:
+sudo /opt/bitnami/ctlscript.sh restart
+
+## Pass below arguments to restart independent services
+sudo /opt/bitnami/ctlscript.sh restart apache
