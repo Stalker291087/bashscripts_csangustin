1792a6197dd4bb1e0c40be0f72e8b42f411fb444
jean <jeancarloe01@hotmail.com>
2024-04-14 09:29:35 -0600

    removing files outside directories

--- push_goaccess_statistics.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-sudo goaccess /opt/bitnami/apache2/logs/access_log --log-format='%h %^[%d:%t %^] "%r" %s %b' --date-format=%d/%b/%Y --time-format=%H:%M:%S -a -o /home/bitnami/apps/moodle/htdocs/report.html
